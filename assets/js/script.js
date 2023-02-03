64e65358552aa60ccf2275a0c8407572c68b914b
Ailsadm <ailsamcgowan@hotmail.co.uk>
2023-02-03 20:47:04 +0000

    name generated for button2 using API and correct answer collected from API for button1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
 let randomQuoteAPI = "https://api.quotable.io/random";
 let nameGeneratorAPI = "https://api.api-ninjas.com/v1/celebrity?name=";
 let quoteDisplay = document.querySelector("#quoteDisplay");
+let NameButton1 = document.querySelector("#NameButton1");
+let NameButton2 = document.querySelector("#NameButton2");
+let NameButton3 = document.querySelector("#NameButton3");
+
+
+// common name generation from array
+let commonNamesArray =["michael", "Steve", "Jennifer", "Martin", "William", "Charlotte", "Mary", "James", "Robert", "David", "Elizabeth", "Sarah"]
+let randomName = commonNamesArray[Math.floor(Math.random()*commonNamesArray.length)]
 
 
 
-    fetch("https://api.api-ninjas.com/v1/celebrity?name=michael",
+// function to grab a celebrity name from the api
+function randomCelebrityNameGenerator(){
+    fetch("https://api.api-ninjas.com/v1/celebrity?name=" + randomName,
       {headers:{"X-Api-Key": "HM4MOd3dfGVsQX8AqXnjlw==ElN28B6ZXeTJkHQM"}}
      )
-.then(response => response.json())
-.then(data => console.log(data))
+     .then(response => response.json())
+     .then(function (result) {
+        //  console.log(result)
+
+let randomCelebrityNameArray = result;
+
+let randomCelebrityName = randomCelebrityNameArray[Math.floor(Math.random()*randomCelebrityNameArray.length)];
+
+let celebrityName = randomCelebrityName.name;
+    
+    NameButton2.textContent = celebrityName;
+
+})
+}
+
+randomCelebrityNameGenerator()
 
 displayQuote()
 
@@ -16,8 +40,10 @@ function displayQuote(){
     fetch(randomQuoteAPI)
     .then(response => response.json())
     .then(function (result) {
-        console.log(result)
+        // console.log(result)
 
-        quoteDisplay.textContent = result.content
+        quoteDisplay.textContent = result.content;
+        NameButton1.textContent = result.author;
     });
 }
+
