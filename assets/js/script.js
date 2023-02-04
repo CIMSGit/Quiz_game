f5f8916987b84e0991f3f5b6ea0e0151ec8e8748
Mcc <macharitocruzuk@gmail.com>
2023-02-04 13:07:10 +0000

    further update readme.md

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,20 @@ let randomQuoteAPI = "https://api.quotable.io/random";
 let nameGeneratorAPI = "https://api.api-ninjas.com/v1/celebrity?name=";
 let quoteDisplay = document.querySelector("#quoteDisplay");
 
+fetch("https://api.api-ninjas.com/v1/celebrity?name=michael", {
+  headers: { "X-Api-Key": "HM4MOd3dfGVsQX8AqXnjlw==ElN28B6ZXeTJkHQM" },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
+displayQuote();
 
-    fetch("https://api.api-ninjas.com/v1/celebrity?name=michael",
-      {headers:{"X-Api-Key": "HM4MOd3dfGVsQX8AqXnjlw==ElN28B6ZXeTJkHQM"}}
-     )
-.then(response => response.json())
-.then(data => console.log(data))
-
-displayQuote()
-
-function displayQuote(){
-    fetch(randomQuoteAPI)
-    .then(response => response.json())
+function displayQuote() {
+  fetch(randomQuoteAPI)
+    .then((response) => response.json())
     .then(function (result) {
-        console.log(result)
+      console.log(result);
 
-        quoteDisplay.textContent = result.content
+      quoteDisplay.textContent = result.content;
     });
 }
